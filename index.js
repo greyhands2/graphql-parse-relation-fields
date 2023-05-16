39c0ce870a936d3f1082041d1866a7199c0abb60
greyhands2 <greyhands2@gmail.com>
2023-05-16 14:56:40 +0100

    first official release

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 const objectFactory=({arr, dbRelationalFields, type})=>{
     let res = {[type]:{}};
-    
-    arr.forEach((selection) =>{
+    for(let index=0; index < arr.length; index++){
         
+        let selection = arr[index]
+        
+        if(selection.kind === "FragmentSpread") continue;
         let currentField = selection["name"]["value"];
+        
         if(!!dbRelationalFields.includes(currentField)===false){
             res[type][currentField] = true;
         } else {
             res[type][currentField] = {...objectFactory({arr:selection.selectionSet.selections, dbRelationalFields, type})};
         }
-        
-    });
+    }
+
        
-      return res;
+    return res;
     
 }
 
 
 const gprf = ({info, dbRelationalFields, type}) => {
-    return objectFactory({arr: info.fieldNodes[0].selectionSet.selections, dbRelationalFields, type});
+    let result = {}
+    let fragArray = Object.keys(info.fragments)
+    if(fragArray.length > 0){
+     
+        fragArray.forEach((fragment)=>{
+            let res = objectFactory({arr: info.fragments[fragment].selectionSet.selections, dbRelationalFields, type});
+            result = preventResultOverwrite(result, type, res)
+           
+        })
+    } 
+    
+    
+    let res = objectFactory({arr: info.fieldNodes[0].selectionSet.selections, dbRelationalFields, type})
+    result = preventResultOverwrite(result, type, res)
+    
+    return result
+}
+
+
+const preventResultOverwrite = (result, type, res) => {
+    if(result[type]){
+        result[type] = {...result[type], ...res[type]}
+    } else {
+        result = {...result,...res}
+    }
+    return result
 }
 module.exports = gprf;
 
